38dcd8df85f199bcd51392d61d854fcf2715c36f
Lee-DongSeop <76763417+dongseoplee@users.noreply.github.com>
2022-04-26 17:39:36 +0900

    add comment

--- practice/src/main/java/myPackage/Assignment.java
+++ practice/src/main/java/myPackage/Assignment.java
@@ -1,8 +1,13 @@
 package myPackage;
 
-public class Assignment {
+public class Assignment(String urlId) {
     private String name;
     private String time;
+    public String courseViewURL = "https://cyber.gachon.ac.kr/course/view.php?id="; //이 주소 String에 + urlId 추가해서 접속
+
+    public Assignment(String urlId) {
+
+    }
 
     public String getName() {
         return name;
@@ -19,4 +24,6 @@ public class Assignment {
     public void setTime(String time) {
         this.time = time;
     }
+
+    //Course에서 id만 가져오기
 }
--- practice/src/main/java/myPackage/Main.java
+++ practice/src/main/java/myPackage/Main.java
@@ -12,6 +12,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String loginURL = "https://cyber.gachon.ac.kr/login/index.php";
         String parsingURL = "https://cyber.gachon.ac.kr/";
+        String courseViewURL = "https://cyber.gachon.ac.kr/course/view.php?id="; //이 주소 String에 + urlId 추가해서 접속
         //로그인 지우기!!
         String id = "";
         String password = "";
@@ -37,18 +38,30 @@ public class Main {
         //수강 강의 리스트 만들어 저장
         List<Course> courseList = new ArrayList<>();
 
+        //elements에 강의 마다 링크 들어있음
         Elements elements = document.select("a.course_link");
 
         System.out.println("elements = " + elements);
         //Course 클래스에 강의 이름과 URL insert
+        //elements에 총 강의수(8개)
         for (Element element : elements) {
 
             String courseName = element.select("h3").text();
             String courseURL = element.attr("abs:href");
 
             Course course = new Course(courseName, courseURL);
-            courseList.add(course);
+            courseList.add(course); //데이터 타입: Course (class)
         }
+
+
+        //과목이름 url
+        //list  url : 123 , 과목이름: 123 학수번호 : 123
+
+        //for 문
+        //접속해야하는 url + id
+        //assigment
+
+
 
         for (Course course : courseList) {
             System.out.println(course.toString());
@@ -61,6 +74,14 @@ public class Main {
                     .timeout(1000)
                     .get();
         }
+
+        List<Assignment> assignmentList = new ArrayList<>();
+        //urlId를 Assginment에 넘겨주는 for문
+        for (Course course : courseList) {
+            String urlId = course.getUrlId();
+            Assignment assignment = new Assignment(urlId);
+        }
+        System.out.println();
         System.out.println("\t" + "!!!!!!!!!!!!!!!!!!!!!Warning!!!!!!!!!!!!!!!!!!!!!!!!");
         System.out.println("\t" + "!!!!!!!                                      !!!!!!!");
         System.out.println("\t" + "!!!!!!!Delete id, password before push to git!!!!!!!");
